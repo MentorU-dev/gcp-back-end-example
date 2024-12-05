deee40447b94237476569da77431852427de2799
Pablo Oyarce <pablooyarceramirez@gmail.com>
2024-12-05 15:56:52 -0300

    fix deploysh

--- deploy.sh
+++ deploy.sh
@@ -9,17 +9,17 @@ export $(grep -v '^#' $BRANCH_NAME.env_deploy | xargs)
 export NOW=$(date '+%Y%m%d%H%M%S')
 export VERSION=$1
 
-export SED_STRING="
-  s/{ARTIFACT_REPOSITORY}/$ARTIFACT_REPOSITORY/g;\
-  s/{SERVICE_NAME}/$SERVICE_NAME/g;\
-  s/{REGION}/$REGION/g;\
-  s/{CICD_PROJECT_ID}/$CICD_PROJECT_ID/g;\
-  s/{VERSION}/$VERSION/g;"
+# Reemplazar placeholders en el archivo de plantilla
+sed -e "s/{ARTIFACT_REPOSITORY}/$ARTIFACT_REPOSITORY/g" \
+    -e "s/{SERVICE_NAME}/$SERVICE_NAME/g" \
+    -e "s/{REGION}/$REGION/g" \
+    -e "s/{CICD_PROJECT_ID}/$CICD_PROJECT_ID/g" \
+    -e "s/{VERSION}/$VERSION/g" \
+    deployment_template.yaml > deployment.yaml
 
-echo $SED_STRING;
-sed $SED_STRING deployment_template.yaml > deployment.yaml;
+# Mostrar el contenido del archivo generado (opcional, para depuración)
 cat deployment.yaml
 
-gcloud beta run services replace deployment.yaml --platform managed --region $REGION 
+gcloud beta run services replace deployment.yaml --platform managed --region "$REGION" 
 
 #gcloud run services update $SERVICE_NAME --vpc-connector $VPC_CONNECTOR --region $REGION
