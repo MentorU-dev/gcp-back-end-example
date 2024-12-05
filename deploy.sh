7164981a0d075fc0990971ed1d9b3323a58d06b6
Pablo Oyarce <pablooyarceramirez@gmail.com>
2024-12-05 14:35:22 -0300

    first

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,12 @@ export $(grep -v '^#' $BRANCH_NAME.env_deploy | xargs)
 export NOW=$(date '+%Y%m%d%H%M%S')
 export VERSION=$1
 
-export SED_STRING="s/{ARTIFACT_REPOSITORY}/$ARTIFACT_REPOSITORY/g;s/{SERVICE_NAME}/$SERVICE_NAME/g;s/{REGION}/$REGION/g;s/{CICD_PROJECT_ID}/$CICD_PROJECT_ID/g;s/{VERSION}/$VERSION/g;"
+export SED_STRING="
+  s/{ARTIFACT_REPOSITORY}/$ARTIFACT_REPOSITORY/g;\
+  s/{SERVICE_NAME}/$SERVICE_NAME/g;\
+  s/{REGION}/$REGION/g;\
+  s/{CICD_PROJECT_ID}/$CICD_PROJECT_ID/g;\
+  s/{VERSION}/$VERSION/g;"
 
 echo $SED_STRING;
 sed $SED_STRING deployment_template.yaml > deployment.yaml;
